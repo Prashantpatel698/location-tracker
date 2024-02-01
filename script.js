868775a7b92c1af55ec223a140d24375292f0a74
Prashantpatel698 <prashantkirar698@gmail.com>
2024-02-02 03:15:19 +0530

    location

---
+++ script.js
@@ -0,0 +1,13 @@
+if("geolocation" in navigator)
+{
+    navigator.geolocation.getCurrentPosition(function(position){
+        let lat =position.coords.latitude;
+        let long =position.coords.longitude;
+        console.log(lat+" "+long)
+
+    })
+}
+else{
+   alert("behan ke lavde location chalu karna re")
+}
+console.log(navigator)
